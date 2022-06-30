748c943f0e48a60ad285894269878a334ba74899
Ben Derham <1805685+benderham@users.noreply.github.com>
2022-06-30 11:12:07 +0800

    remove unwanted files, install Next.js

---
+++ apps/web/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const withPreconstruct = require('@preconstruct/next');
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withPreconstruct(nextConfig);
